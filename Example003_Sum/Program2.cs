3144f1ecaa558b805e0d40a696f9fd68e0808ecb
Andrew <kladko_04@mail.ru>
2022-08-28 16:48:54 +0500

    Third

--- Example003_Sum/Program2.cs
+++ Example003_Sum/Program2.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(1,10); // 0,1,2,3,4,5,6,7,8,9
+﻿int numberA = new Random().Next(1,11); // 0,1,2,3,4,5,6,7,8,9, 10
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);// тот же набор чисел
+int numberB = new Random().Next(1,11);// тот же набор чисел
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
