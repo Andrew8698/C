8ed2ca331b74c8ea24e77dc61991c63787547a91
Andrew <kladko_04@mail.ru>
2022-08-28 16:47:20 +0500

    Fifth

---
+++ Example005_If_Else/Program4.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользовтеля: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Оу, это Маша..");
+}
+else
+{
+    Console.Write("Здарова, ");
+    Console.WriteLine(username);
+
+}
